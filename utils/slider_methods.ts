0cc62b6369a3dddac2f4bfc10483140d0f63f578
Dagmawi Nebiat <dagmnebiat@gmail.com>
2026-03-03 19:54:16 +0300

    removed comments of temp code

--- utils/slider_methods.ts
+++ utils/slider_methods.ts
@@ -5,8 +5,7 @@ export const slideToDate = (
   month?: number,
   year?: number,
 ) => {
-  // if (!month || !year) {
-  if (true) {
+  if (!month || !year) {
     setError("Could not slide to exact date");
 
     setTimeout(() => {
@@ -16,17 +15,17 @@ export const slideToDate = (
     return;
   }
 
-  // const dateTickElement = document.getElementById(
-  //   `${year}-${month > 9 ? month : `0${month}`}-01`,
-  // );
+  const dateTickElement = document.getElementById(
+    `${year}-${month > 9 ? month : `0${month}`}-01`,
+  );
 
-  // if (!dateTickElement) return;
+  if (!dateTickElement) return;
 
-  // dateTickElement.scrollIntoView({
-  //   behavior: "smooth",
-  //   block: "center",
-  //   inline: "center",
-  // });
+  dateTickElement.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+    inline: "center",
+  });
 };
 
 export const updateCurrentDateFromScroll = (
