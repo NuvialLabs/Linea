fa34c9e048876bfbeef6730d833e1b50801ec3e3
Dagmawi Nebiat <dagmnebiat@gmail.com>
2026-03-17 10:41:16 +0300

    Implemented the account menu
    - Menu items include; sync, settings, logout
    - Sync and Logout functionality are pending

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,8 +11,10 @@ import TimelineStore from "@/stores/timeline-store";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
-  const [syncDate, setSyncDate] = useState<Date | null>(new Date("2/3/2026")); //TODO: replace with actual last sync date
   const { error, setError } = TimelineStore();
+
+  const [syncDate, setSyncDate] = useState<Date | null>(new Date("2/3/2026")); //TODO: replace with actual last sync date
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
 
   return (
     <main className=" w-screen grid place-items-between justify-items-center">
@@ -35,13 +37,33 @@ export default function Home() {
             <h1 className="text-(--secondary-foreground) text-[10px] sm:text-[14px] text-end">
               {syncDate
                 ? `Last synced: ${syncDate.toLocaleDateString()} ${syncDate.toLocaleTimeString()}`
-                : "Not synced yet"}
+                : "Not synced yet"}{" "}
+              {/* //TODO: Only show when there is a Google drive account to sync */}
+              with
             </h1>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSle5CxW6QjBz4FH6p5szdloz2gPoQLJ8Outg&s" //TODO: replace with actual profile image from Google Account
-              alt="profile"
-              className="w-12 h-12 rounded-full bg-(--secondary-foreground)/20"
-            />
+
+            <div className="w-20 relative">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSle5CxW6QjBz4FH6p5szdloz2gPoQLJ8Outg&s" //TODO: replace with actual profile image from Google Account
+                alt="profile"
+                className="w-12 h-12 rounded-full bg-(--secondary-foreground)/20 cursor-pointer"
+                onClick={() => setIsMenuExpanded(!isMenuExpanded)}
+              />
+
+              {isMenuExpanded && (
+                <>
+                  <nav className="absolute w-48 h-42 grid justify-center gap-5 p-5 border-(--accent) border right-0 top-15 rounded-xl text-(--accent)">
+                    <button className="cursor-pointer">Sync</button>
+                    <Link href="/settings">Settings</Link>
+                    <button className="cursor-pointer">Logout</button>
+                  </nav>
+                  <div
+                    className="w-screen h-screen fixed top-0 left-0"
+                    onClick={() => setIsMenuExpanded(false)}
+                  />
+                </>
+              )}
+            </div>
           </aside>
         </div>
       </nav>
