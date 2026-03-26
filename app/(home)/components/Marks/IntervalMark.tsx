75d3c1f45c839c3d26def63ecf119f4ded461808
Dagmawi Nebiat <dagmnebiat@gmail.com>
2026-03-26 16:47:46 +0300

    Delete event logic added in the point and interval marks
    - Refactored event item variable to be mapped to `currentEvent`
    - Imported PencilIcon and TrashIcon
    - Fixed the dynamic width enlargement issue with useLayoutEffect

--- app/(home)/components/Marks/IntervalMark.tsx
+++ app/(home)/components/Marks/IntervalMark.tsx
@@ -1,6 +1,6 @@
 import TimelineStore from "@/stores/timeline-store";
-import { Event } from "@/global/types";
-import { useEffect, useRef, useState } from "react";
+import { Event, Timeline } from "@/global/types";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { checkIfImage, onAttachementClick } from "./utils";
 import {
   ArrowsPointingInIcon,
@@ -8,11 +8,21 @@ import {
   ArrowTopRightOnSquareIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
+  PencilIcon,
+  TrashIcon,
 } from "@heroicons/react/16/solid";
 import { differenceInDays } from "@/utils/date_methods";
+import { useDrive } from "@/hooks/useDrive";
 
 const IntervalMark = ({ events }: { events: Event[] }) => {
-  const { zoomOptions } = TimelineStore();
+  const {
+    zoomOptions,
+    timelines,
+    setTimelines,
+    selectedTimeline,
+    setSelectedTimeline,
+    setLastUpdatedToDrive,
+  } = TimelineStore();
 
   const [eventIndex, setEventIndex] = useState(0);
   const [containerWidth, setContainerWidth] = useState<string | undefined>(
@@ -21,37 +31,43 @@ const IntervalMark = ({ events }: { events: Event[] }) => {
   const [isImage, setIsImage] = useState(false);
   const [isEnlarged, setIsEnlarged] = useState(false);
   const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const { saveData } = useDrive();
+
+  const currentEvent = events[eventIndex];
   const isExpandable =
-    (events[eventIndex]?.description?.length ?? 0) > 82 ||
-    events[eventIndex].link !== undefined;
+    (currentEvent?.description?.length ?? 0) > 82 ||
+    currentEvent.link !== undefined;
   const dateDifference = differenceInDays(
-    events[eventIndex].endDate!,
-    events[eventIndex].initialDate,
+    currentEvent.endDate!,
+    currentEvent.initialDate,
   );
+
   const intervalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (events[eventIndex].link) {
-      checkIfImage(events[eventIndex].link).then((res) => {
+    if (currentEvent.link) {
+      checkIfImage(currentEvent.link).then((res) => {
         setIsImage(res);
       });
     }
-
-    setContainerWidth(
-      (intervalRef.current?.offsetWidth ?? 0) > 200 ? "100%" : undefined,
-    );
   }, [eventIndex]);
 
-  useEffect(() => {
-    setContainerWidth(
-      (intervalRef.current?.offsetWidth ?? 0) < 58 ? "100%" : undefined,
-    );
-  }, []);
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      if (!intervalRef.current) return;
+
+      console.log(intervalRef.current.offsetWidth);
+      setContainerWidth(
+        intervalRef.current.offsetWidth < 100 ? "100%" : undefined,
+      );
+    }, 1000);
+  }, [eventIndex, isEnlarged]);
 
   return (
     <div className="relative group">
       <div className="grid justify-items-center gap-2 absolute z-30 -top-12 left-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        <h1 className="text-(--secondary-foreground)/70 text-xs text-center)">{`${events[eventIndex].initialDate.getMonth() + 1}/${events[eventIndex].initialDate.getDate()} - ${events[eventIndex].endDate!.getMonth() + 1}/${events[eventIndex].endDate!.getDate()}`}</h1>
+        <h1 className="text-(--secondary-foreground)/70 text-xs text-center)">{`${currentEvent.initialDate.getMonth() + 1}/${currentEvent.initialDate.getDate()} - ${currentEvent.endDate!.getMonth() + 1}/${currentEvent.endDate!.getDate()}`}</h1>
         {events.length > 1 && (
           <div className=" h-5 w-20 rounded-full text-white bg-(--secondary-foreground)/20 flex justify-between items-center px-1 ">
             <ChevronLeftIcon
@@ -90,8 +106,7 @@ const IntervalMark = ({ events }: { events: Event[] }) => {
             ? (containerWidth ??
               `${dateDifference * 12 * (1 + zoomOptions.level / 50)}px`)
             : `${dateDifference * 12 * (1 + zoomOptions.level / 50)}px`,
-          backgroundColor:
-            events[eventIndex].color ?? "var(--primary-foreground)",
+          backgroundColor: currentEvent.color ?? "var(--primary-foreground)",
         }}
         onWheel={(e) => {
           e.stopPropagation();
@@ -105,30 +120,71 @@ const IntervalMark = ({ events }: { events: Event[] }) => {
       >
         <div className="flex justify-between p-2">
           <h1 className="font-bold line-clamp-2 text-ellipsis">
-            {events[eventIndex].name}
+            {currentEvent.name}
           </h1>
-          {isExpandable && isEnlarged && (
-            <ArrowsPointingInIcon
-              className="w-4 h-4 cursor-pointer"
-              onMouseDown={() => setIsEnlarged(false)}
-            />
-          )}
 
-          {isExpandable && !isEnlarged && (
-            <ArrowsPointingOutIcon
-              className="w-4 h-4 cursor-pointer"
-              onMouseDown={() => setIsEnlarged(true)}
-            />
-          )}
+          <div className="flex items-center gap-3">
+            {isEnlarged && (
+              <PencilIcon
+                className="w-4 h-4 cursor-pointer"
+                onMouseDown={() => {}}
+              />
+            )}
+            {isEnlarged && (
+              <TrashIcon
+                onMouseDown={async () => {
+                  if (selectedTimeline === null) return;
+
+                  const updatedTimeline: Timeline = {
+                    ...selectedTimeline,
+                    events: selectedTimeline.events.filter(
+                      (event) => event.id !== currentEvent.id,
+                    ),
+                  };
+                  const updatedTimelines = timelines.map((timeline) => {
+                    if (timeline.id === selectedTimeline.id) {
+                      return updatedTimeline;
+                    }
+                    return timeline;
+                  });
+
+                  setSelectedTimeline(updatedTimeline);
+                  setTimelines(updatedTimelines);
+                  setEventIndex(0);
+
+                  const result = await saveData(updatedTimelines);
+
+                  if (result) {
+                    setLastUpdatedToDrive(new Date(result.modifiedTime));
+                  }
+                }}
+                className="w-4 h-4 cursor-pointer"
+              />
+            )}
+
+            {isExpandable && isEnlarged && (
+              <ArrowsPointingInIcon
+                className="w-4 h-4 cursor-pointer"
+                onMouseDown={() => setIsEnlarged(false)}
+              />
+            )}
+
+            {isExpandable && !isEnlarged && (
+              <ArrowsPointingOutIcon
+                className="w-4 h-4 cursor-pointer"
+                onMouseDown={() => setIsEnlarged(true)}
+              />
+            )}
+          </div>
         </div>
 
         {isEnlarged && (
           <p className="text-[12px] mt-1.5 break-all px-2">
-            {events[eventIndex].description}
+            {currentEvent.description}
           </p>
         )}
 
-        {events[eventIndex].link && (
+        {currentEvent.link && (
           <div
             className={`w-full ${isEnlarged ? (isImage ? "h-41" : "h-10") : "h-0"} mt-8 duration-300`}
             style={{
@@ -141,18 +197,18 @@ const IntervalMark = ({ events }: { events: Event[] }) => {
             {isEnlarged &&
               (isImage ? (
                 <img
-                  src={events[eventIndex].link}
+                  src={currentEvent.link}
                   alt=""
                   className="object-cover h-41 w-full"
-                  onMouseDown={(e) => onAttachementClick(e, events[eventIndex])}
+                  onMouseDown={(e) => onAttachementClick(e, currentEvent)}
                 />
               ) : (
                 <button
-                  onMouseDown={(e) => onAttachementClick(e, events[eventIndex])}
+                  onMouseDown={(e) => onAttachementClick(e, currentEvent)}
                   className="h-6 w-[96%] bg-(--secondary-foreground)/20 rounded-md flex items-center justify-between px-2 py-4 m-1 cursor-pointer"
                 >
                   <h1 className="w-[85%] line-clamp-2 text-xs text-start text-ellipsis">
-                    {events[eventIndex].link}
+                    {currentEvent.link}
                   </h1>
                   <ArrowTopRightOnSquareIcon className="h-4" />
                 </button>
